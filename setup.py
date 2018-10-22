632755c6f0fdfac11640960c9cc8033951d6e4b2
Anna Dvoretskaya <anja.dvr@gmail.com>
2018-10-22 16:17:13 +0300

    Update setup.py
    Changed pika requirement version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-getemail',
-    version='0.1.5',
+    version='0.1.6',
     packages=find_packages(),
     include_package_data=True,
     license='BSD License',
@@ -20,7 +20,7 @@ setup(
     author_email='anna.d@razortheory.com',
     install_requires=[
             'Django>=1.11',
-            'pika==0.11.2'
+            'pika==0.12.0'
         ],
     classifiers=[
         'Environment :: Web Environment',
